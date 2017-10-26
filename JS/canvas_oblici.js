a4cc6e8555aee9ab4239ba3d70933c03d8450526
smusa92 <smusa92@users.noreply.github.com>
2017-10-26 16:17:37 +0200

    Ship and stars
    Od početne igrice napravljema malo složenija igrica koja sa svenirskim brodom hvata zvijezde po mapi...

--- JS/canvas_oblici.js
+++ JS/canvas_oblici.js
@@ -1,39 +1,27 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var ballRadius=10;
 
-var x=canvas.width/2;
-var y=canvas.height-30;
-var dx=2;
-var dy=-2;
+star = new Image();
+star.src = 'https://constellation.standuptocancer.org/assets/star-facbd92545e601590a94bd3fd813f643.png';
 
-var paddleHeight=10;
-var paddleWidth=75;
-var paddleX=(canvas.width-paddleWidth)/2;
+shipImg = new Image();
+shipImg.src = 'https://jackrugile.com/images/misc/rocket-sprite.png';
 
-var rightPressed= false;
-var leftPressed=false;
-
-var brickRowCount = 4;
-var brickColumnCount = 5;
-var brickWidth = 75;
-var brickHeight = 20;
-var brickPadding = 5;
-var brickOffsetTop = 30;
-var brickOffsetLeft = 40;
+var osX=750;
+var randomStarY = Math.floor(Math.random() * 480) + 15 ;
 
 var score = 0;
+var starScore = 0;
+
+var rightPressed =leftPressed =upPressed =downPressed = false;
+
+var shipWidth = shipImg.width;
+var shipHeight = shipImg.height;
+var shipX=(canvas.width-shipWidth)/2;
+var shipY=((canvas.height-shipHeight)/2)-5;
 
 document.addEventListener("keydown",keyDownHandler,false);
 document.addEventListener("keyup",keyUpHandler,false);
-
-var bricks = [];
-for(c=0; c<brickColumnCount; c++) {
-    bricks[c] = [];
-    for(r=0; r<brickRowCount; r++) {
-         bricks[c][r] = { x: 0, y: 0, status: 1 };
-    }
-}
 
 function keyDownHandler(e){
 	if(e.keyCode==39){	
@@ -42,118 +30,87 @@ function keyDownHandler(e){
 	else if(e.keyCode==37){	
 		leftPressed=true;
 		}
+        else if(e.keyCode==38){	
+		upPressed=true;
+		}
+        else if(e.keyCode==40){	
+		downPressed=true;
+		}
+                else if(e.keyCode==80){
+		alert("PAUSE! \n Press OK to continue.");
+		}
 	}
-	
 function keyUpHandler(e){
 	
 	if(e.keyCode==39){
-		
 		rightPressed=false;
 	}
 	else if(e.keyCode==37){
 		leftPressed=false;
 		}
+        else if(e.keyCode==38){	
+		upPressed=false;
+		}
+        else if(e.keyCode==40){	
+		downPressed=false;
+		}
 	}
-
-function drawBall(){
-	ctx.beginPath();
-	ctx.arc(x,y,ballRadius,0,2*Math.PI);
-	ctx.fillstyle="#0033FF";
-	ctx.fillStroke="#0033FF";
-	ctx.Stroke="10"
-	ctx.fill();
-	ctx.closePath();
-	}
-
-function drawPaddle(){
-	ctx.beginPath();
-	ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
-	ctx.fillstyle="#0095DD";
-	ctx.fill();
-	ctx.closePath();
-	}
-function drawBricks() {
-    for(c=0; c<brickColumnCount; c++) {
-        for(r=0; r<brickRowCount; r++) {
-            if(bricks[c][r].status == 1) {
-                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-                bricks[c][r].x = brickX;
-                bricks[c][r].y = brickY;
-                ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "#0095DD";
-                ctx.fill();
-                ctx.closePath();
-            }
-        }
-    }
+function drowStar(){
+    ctx.beginPath();
+    ctx.drawImage(star,osX, randomStarY, 21, 21);
+    ctx.closePath();
+    ctx.restore();
+    osX--;
 }
+
+function drowShip(){
+	ctx.beginPath();
+        ctx.drawImage( shipImg, shipX, shipY,40,30);
+	ctx.closePath();
+	}
+
 function drawScore() {
     ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText("Score: "+score, 8, 20);
+    ctx.fillStyle = "#00FFFF";
+    ctx.fillText("Score: "+score+"/"+starScore, 8, 20);
 }
+function newStar(){
+    osX=750;
+    randomStarY = Math.floor(Math.random() * 480) + 15 ;
+       drowStar();
+}
+
 function collisionDetection() {
-    for(c=0; c<brickColumnCount; c++) {
-        for(r=0; r<brickRowCount; r++) {
-            var b = bricks[c][r];
-            if(b.status == 1) {
-                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-                    dy = -dy;
-                    b.status = 0;
-                    score++;
-                    if(score == brickRowCount*brickColumnCount) {
-                        alert("YOU WIN, CONGRATULATIONS!");
-                        document.location.reload();
-                    }
-                }
-            }
-        }
+	if(shipX+20>osX && shipX<osX+21 && shipY+20>randomStarY && shipY<randomStarY+21){
+            //ctx.clearRect(osX-750, randomStarY, 21, 21);
+            newStar();
+        score++;
     }
 }
-
-
 
 function draw(){
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-        drawScore()
-	drawBricks();
-	drawBall();
-	drawPaddle();
-	collisionDetection();
-	// ako  je vrijednost x u toj točki manja od širine prozora cancas promjeni suprotan smjer
-    // samim tim što dajemo nagativan predznak gibajnja lopta se giba u suprotnom smjeru
- if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-    if(y + dy < ballRadius) {
-        dy = -dy;
-    }
-    else if(y + dy > canvas.height-ballRadius) {
-        if(x > paddleX && x < paddleX + paddleWidth) {
-			 if(y= y-paddleHeight){
-            dy = -dy  ;
-			 }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drowShip();
+    drowStar();
+    drawScore();
+    collisionDetection();
+    if(osX<10){
+     newStar();
         }
-        else {
-            alert("GAME OVER");
-            document.location.reload();
-        }
+        if(osX<750 && osX>748){
+    starScore++;
     }
-	if(rightPressed && paddleX<canvas.width-paddleWidth){
-		
-		paddleX+=7;
-		}
-	 else if(leftPressed && paddleX>0 ){
-		 paddleX-=7;
-		 
-		 }
-		 
-		 x=x+dx;
-	     y=y+dy;
-	}
-
-// funkcija koja intervalno svakih npr 10 ms pokreće finkciju draw
-//što je veći broj to se loptica sporije kreće
-setInterval(draw, 13);
+    if(rightPressed && shipX < canvas.width){
+        shipX += 3;
+    }
+    else if(leftPressed && shipX > 0){
+        shipX -= 3;
+    }
+    else if(upPressed && shipY > 10){
+        shipY -= 3;
+    }
+    else if(downPressed && shipY < 520){
+        shipY += 3;
+    }
+}
+setInterval(draw,10);
